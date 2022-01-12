de07fdc785eb1618e987ad0cba1a613c2da31bba
Choene <cchokoe@yahoo.com>
2022-01-12 02:31:33 +0200

    DewayDecimal system of classificasion class model properties

---
+++ eBookManager.Engine/DeweyDecimal.cs
@@ -0,0 +1,17 @@
+﻿namespace eBookManager.Engine
+{
+    public class DeweyDecimal
+    {
+        public string Generalities { get; set; } = "000";
+        public string PhilosophyAndPsychology { get; set; } = "100";
+        public string Religion { get; set; } = "200";
+        public string SocialSciences { get; set; } = "300";
+        public string Language { get; set; } = "400";
+        public string NaturalSciencesAndMathematics { get; set; } = "500";
+        public string TechnologyAndAppliedSciences { get; set; } = "600";
+        public string TheArts { get; set; } = "700";
+        public string Literature { get; set; } = "800";
+        public string GeographyAndHistory { get; set; } = "900";
+
+    }
+}
