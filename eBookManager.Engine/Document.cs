543dc81ba8f36a638a5c147b499bebd5088a2f19
Choene <cchokoe@yahoo.com>
2022-01-12 02:01:47 +0200

    Document class model properties representing a sigle book

--- eBookManager.Engine/Document.cs
+++ eBookManager.Engine/Document.cs
@@ -6,5 +6,19 @@ namespace eBookManager.Engine
 {
     public class Document
     {
+        public string Title { get; set; }
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+        public DateTime LastAccessed { get; set; }
+        public DateTime Created { get; set; }
+        public String FilePath { get; set; }
+        public String FileSize { get; set; }
+        public String ISBN { get; set; }
+        public String Price { get; set; }
+        public String Publisher { get; set; }
+        public String Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DeweyDecimal Classification { get; set; }
+        public String Category { get; set; }
     }
 }
