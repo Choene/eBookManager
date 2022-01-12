91ac734912a49f66973140c127a3a8b126dd8509
Choene <cchokoe@yahoo.com>
2022-01-12 03:21:55 +0200

    ToInt() class extension method

--- eBookManager.Helper/ExtensionMethods.cs
+++ eBookManager.Helper/ExtensionMethods.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using eBookManager.Engine;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace eBookManager.Helper
 {
     public static class ExtensionMethods
     {
+        //ToInt() class extension method to take a string value and try parse it to an integer value
+        public static int ToInt(this string value, int defaultInteger = 0)
+        {
+            try
+            {
+                if (int.TryParse(value, out int validInteger))
+                {
+                    //Out variables
+                    return validInteger;
+                }
+                else
+                {
+                    return defaultInteger;
+                }
+            }
+            catch
+            {
+
+                return defaultInteger;
+            }
+        }
     }
 }
