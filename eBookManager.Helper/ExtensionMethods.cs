692f83a7bc9f4fb1efec841675db4415f64a8710
Choene <cchokoe@yahoo.com>
2022-01-12 03:24:29 +0200

    StorageSpaceExists() class extension method

--- eBookManager.Helper/ExtensionMethods.cs
+++ eBookManager.Helper/ExtensionMethods.cs
@@ -29,7 +29,24 @@ namespace eBookManager.Helper
                 return defaultInteger;
             }
         }
+
         //ToMegabytes method
         public static double ToMegabytes(this long bytes) => (bytes > 0) ? (bytes / 1024f) / 1024f : bytes;
+
+        //Check whether storage space already exist StorageSpaceExist()
+        public static bool StorageSpaceExists(this List<StorageSpace> space, string nameValueToCheck, out int storageSpaceId)
+        {
+            bool exists = false;
+            storageSpaceId = 0;
+            if (space.Count() != 0)
+            {
+                int count = (from r in space
+                             where r.Name.Equals(nameValueToCheck)
+                             select r).Count();
+                if (count > 0) exists = true;
+                storageSpaceId = (from r in space select r.ID).Max() + 1;
+            }
+            return exists;
+        }
     }
 }
